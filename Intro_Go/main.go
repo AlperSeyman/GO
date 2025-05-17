1874efc036a8da0fa68fd85192fe906540b96d23
Alper Seyman <alperseyman@gmail.com>
2025-05-17 14:58:25 +0200

    4

--- Intro_Go/main.go
+++ Intro_Go/main.go
@@ -1,5 +1,19 @@
 package main
 
+import (
+	"fmt"
+)
+
 func main() {
 
+	statePopulations := map[string]int{ // map[key]value
+		"California": 39250017,
+		"Texas":      27862589,
+		"Florida":    20612439,
+		"New York":   19745289,
+	}
+
+	for _, v := range statePopulations {
+		fmt.Println(v)
+	}
 }
