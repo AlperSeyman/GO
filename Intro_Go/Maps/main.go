8aa9f04106662869b4630489b7296d44a2691acf
Alper Seyman <alperseyman@gmail.com>
2025-05-28 14:37:59 +0200

    added nested map and examples

--- Intro_Go/Maps/main.go
+++ Intro_Go/Maps/main.go
@@ -1,10 +1,28 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+)
+
+func getNameCounts(names []string) map[rune]map[string]int {
+	counts := make(map[rune]map[string]int)
+	for _, name := range names {
+		if len(name) == 0 {
+			continue
+		}
+		firstChar := rune(name[0])
+		_, ok := counts[firstChar]
+		if !ok {
+			counts[firstChar] = make(map[string]int)
+		}
+		counts[firstChar][name]++
+	}
+	return counts
+}
 
 func main() {
 
-	hits := make(map[string]map[string]string)
+	names := []string{"Bily", "Bily", "Bob", "Joe"}
 
-	fmt.Println(hits)
+	fmt.Print(getNameCounts(names))
 }
