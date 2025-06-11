8ab158f4cb0f2c1c565f666e914bb71d7963cb7a
Alper Seyman <alperseyman@gmail.com>
2025-06-11 21:40:01 +0200

    pointer updated

--- Intro_Go/Pointers/main.go
+++ Intro_Go/Pointers/main.go
@@ -4,15 +4,10 @@ import "fmt"
 
 func main() {
 
-	myNumber := 23
-
-	var ptr = &myNumber
-
-	fmt.Println("Value of actual pointer", ptr)
-	fmt.Println("Value of actual pointer", *ptr)
-
-	*ptr = *ptr * 2
-
-	fmt.Println("New value is: ", myNumber)
-
+	var pointer *string
+	str := "hello"
+	pointer = &str
+	fmt.Println(str)
+	*pointer = "changed"
+	fmt.Println(str)
 }
