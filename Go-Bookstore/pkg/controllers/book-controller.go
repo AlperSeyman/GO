60c5a4fb6b39339149a957563415ba29fb6d31c6
Alper Seyman <alperseyman@gmail.com>
2025-07-31 23:02:55 +0200

    completed all controller functions

--- Go-Bookstore/pkg/controllers/book-controller.go
+++ Go-Bookstore/pkg/controllers/book-controller.go
@@ -6,7 +6,9 @@ import (
 	"net/http"
 	"strconv"
 
+	"github.com/AlperSeyman/bookstore/pkg/config"
 	"github.com/AlperSeyman/bookstore/pkg/models"
+	"github.com/AlperSeyman/bookstore/pkg/utils"
 	"github.com/gorilla/mux"
 )
 
@@ -27,6 +29,7 @@ func GetBookById(w http.ResponseWriter, r *http.Request) {
 	bookId, err := strconv.ParseInt(id, 0, 0)
 	if err != nil {
 		log.Fatal(err)
+		return
 	}
 
 	book, _ := models.GetByIdBook(uint(bookId))
@@ -37,8 +40,75 @@ func CreateBook(w http.ResponseWriter, r *http.Request) {
 
 	w.Header().Set("Content-Type", "application/json")
 
-	var newBook models.Book
-	_ = json.NewDecoder(r.Body).Decode(&newBook)
+	var book models.Book
+	err := utils.ParseBody(r, &book)
+	if err != nil {
+		log.Fatal(err)
+		return
+	}
+	newBook := book.CreateBook()
+	json.NewEncoder(w).Encode(newBook)
 
-	newBook.CreateBook()
+}
+
+func DeleteBook(w http.ResponseWriter, r *http.Request) {
+
+	w.Header().Set("Content-Type", "application/json")
+	vars := mux.Vars(r)
+	bookId := vars["bookId"]
+	id, err := strconv.ParseInt(bookId, 0, 0)
+	if err != nil {
+		log.Fatal(err)
+		return
+	}
+
+	book, err := models.DeleteByIdBook(uint(id))
+	if err != nil {
+		log.Fatal(err)
+		return
+	}
+
+	json.NewEncoder(w).Encode(book)
+}
+
+func UpdateBook(w http.ResponseWriter, r *http.Request) {
+
+	w.Header().Set("Content-Type", "application/json")
+
+	vars := mux.Vars(r)
+	bookId := vars["bookId"]
+	id, err := strconv.ParseInt(bookId, 0, 0)
+	if err != nil {
+		log.Fatal(err)
+		return
+	}
+
+	currentBook, err := models.GetByIdBook(uint(id))
+	if err != nil {
+		log.Fatal(err)
+		return
+	}
+
+	var updateBook models.Book
+
+	err = utils.ParseBody(r, &updateBook)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	if updateBook.Author != "" {
+		currentBook.Author = updateBook.Author
+	}
+
+	if updateBook.Name != "" {
+		currentBook.Name = updateBook.Name
+	}
+
+	if updateBook.Publication != "" {
+		currentBook.Publication = updateBook.Publication
+	}
+
+	config.DB.Save(&currentBook)
+	json.NewEncoder(w).Encode(currentBook)
+
 }
