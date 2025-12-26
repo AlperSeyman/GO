c426849cf88f62f3c0efdc70a2f1882cb4da9b0f
Alper Seyman <89801493+AlperSeyman@users.noreply.github.com>
2025-12-26 18:38:28 +0100

    added panic

---
+++ basics/panic.go
@@ -0,0 +1,26 @@
+package basics
+
+import "fmt"
+
+func main() {
+
+	// panic(interface{})
+
+	// Example of a valid input
+	//process(10)
+
+	// Example of a invalid input
+	process(-3)
+}
+
+func process(input int) {
+
+	defer fmt.Println("Deferred 1")
+	defer fmt.Println("Deferred 2")
+
+	if input < 0 {
+		fmt.Println("Before panic")
+		panic("input must be non-negative number")
+	}
+	fmt.Println("Processing input: ", input)
+}
