6ec78638cb53b6d9bf346e04fe0f47a0a01dbf76
Alper Seyman <89801493+AlperSeyman@users.noreply.github.com>
2026-01-07 21:36:13 +0100

    added loadClientCAs function

--- REST API Project/Sımple_API/server.go
+++ REST API Project/Sımple_API/server.go
@@ -2,9 +2,11 @@ package main
 
 import (
 	"crypto/tls"
+	"crypto/x509"
 	"fmt"
 	"log"
 	"net/http"
+	"os"
 
 	"golang.org/x/net/http2"
 )
@@ -12,9 +14,12 @@ import (
 func main() {
 
 	http.HandleFunc("/orders", func(w http.ResponseWriter, r *http.Request) {
+		logRequestDetails(r)
 		fmt.Fprintf(w, "Handling incoming orders")
+
 	})
 	http.HandleFunc("/users", func(w http.ResponseWriter, r *http.Request) {
+		logRequestDetails(r)
 		fmt.Fprintf(w, "Handling users")
 	})
 
@@ -26,6 +31,8 @@ func main() {
 
 	// Configure TLS
 	tlsConfig := &tls.Config{
+		ClientAuth: tls.RequireAndVerifyClientCert, // turns on mTLS
+		ClientCAs:  loadClienCAs(),                 // List of trusted client IDs
 		MinVersion: tls.VersionTLS13,
 	}
 
@@ -51,4 +58,43 @@ func main() {
 	// if err != nil{
 	// 	log.Fatalln("Could not start server:", err)
 	// }
+}
+
+func logRequestDetails(r *http.Request) {
+	httpVersion := r.Proto
+	fmt.Println("Received request with HTTP version:", httpVersion)
+
+	if r.TLS != nil {
+		tlsVersion := getTLSVersion(r.TLS.Version)
+		fmt.Println("Received request with HTTP version: ", tlsVersion)
+	} else {
+		fmt.Println("Received request without TLS.")
+	}
+}
+
+func getTLSVersion(version uint16) string {
+	switch version {
+	case tls.VersionTLS10:
+		return "TLS 1.0"
+	case tls.VersionTLS11:
+		return "TLS 1.1"
+	case tls.VersionTLS12:
+		return "TLS 1.2"
+	case tls.VersionTLS13:
+		return "TLS 1.3"
+	default:
+		return "Unknown TLS version"
+	}
+}
+
+func loadClienCAs() *x509.CertPool { // CertPool store and manage a collection of Trusted Certificates.
+
+	clientCAs := x509.NewCertPool() // hold certificates trust
+
+	caCert, err := os.ReadFile("cert.pem")
+	if err != nil {
+		log.Fatalln("Could not load client CA: ")
+	}
+	clientCAs.AppendCertsFromPEM(caCert)
+	return clientCAs
 }
