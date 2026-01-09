a3fc74052fad43977150232523e73cba015c5951
Alper Seyman <89801493+AlperSeyman@users.noreply.github.com>
2026-01-09 19:38:40 +0100

    added goroutine

--- goroutine.go
+++ goroutine.go
@@ -12,15 +12,50 @@ import (
 
 func main() {
 
+	var err error
+
 	fmt.Println("Beginning Program.")
 	go sayHello()
 	fmt.Println("After sayHello() function.")
 
+	// err = go doWork() // This is not accepted
+	go printLetters()
+	go printNumber()
+
+	go func() {
+		err = doWork()
+	}()
+
+	if err != nil {
+		fmt.Println("Error: ", err)
+	} else {
+		fmt.Println("Work Completed Succesfully")
+	}
 	time.Sleep((2 * time.Second))
-	fmt.Println("Ending Program.")
+
 }
 
 func sayHello() {
 	time.Sleep(2 * time.Second)
 	fmt.Println("Hello from Goroutine.")
+}
+
+func printNumber() {
+	for i := 0; i < 5; i++ {
+		fmt.Println("Number: ", i, time.Now())
+		time.Sleep(100 * time.Millisecond)
+	}
+}
+
+func printLetters() {
+	for _, letter := range "abcde" {
+		fmt.Println("Letter: ", string(letter), time.Now())
+		time.Sleep(200 * time.Millisecond)
+	}
+}
+
+func doWork() error {
+	// Simulate work
+	time.Sleep(1 * time.Second)
+	return fmt.Errorf("an error occured in dowork")
 }
