94c033fe56cf8ab8ff38a9cfe84f619835226653
Alper Seyman <89801493+AlperSeyman@users.noreply.github.com>
2026-01-21 22:17:18 +0100

    updated server.go and added reminder.md file

--- School API Project/cmd/api/server.go
+++ School API Project/cmd/api/server.go
@@ -1,9 +1,7 @@
 package main
 
 import (
-	"encoding/json"
 	"fmt"
-	"io"
 	"log"
 	"net/http"
 )
@@ -14,94 +12,68 @@ type user struct {
 	City string `json:"city"`
 }
 
+func rootHandler(w http.ResponseWriter, r *http.Request) {
+	// fmt.Fprintf(w, "Main Page")
+	w.Write([]byte("Main Page"))
+}
+
+func teachersHandler(w http.ResponseWriter, r *http.Request) {
+	// fmt.Fprintf(w, "Main Page")
+
+	switch r.Method {
+	case http.MethodGet:
+		w.Write([]byte("GET Method on Teachers Route Page"))
+	case http.MethodPost:
+		w.Write([]byte("POST Method on Teachers Route Page"))
+	case http.MethodPut:
+		w.Write([]byte("PUT Method on Teachers Route Page"))
+	case http.MethodDelete:
+		w.Write([]byte("DELETE Method on Teachers Route Page"))
+	default:
+		w.Write([]byte("Teachers Route Page"))
+	}
+}
+
+func studentsHandler(w http.ResponseWriter, r *http.Request) {
+	w.Write([]byte("Student Page"))
+	switch r.Method {
+	case http.MethodGet:
+		w.Write([]byte("GET Method on Students Route Page"))
+	case http.MethodPost:
+		w.Write([]byte("POST Method on Students Route Page"))
+	case http.MethodPut:
+		w.Write([]byte("PUT Method on Students Route Page"))
+	case http.MethodDelete:
+		w.Write([]byte("DELETE Method on Students Route Page"))
+	default:
+		w.Write([]byte("Students Route Page"))
+	}
+}
+
+func execsHandler(w http.ResponseWriter, r *http.Request) {
+	w.Write([]byte("Execs Page"))
+	switch r.Method {
+	case http.MethodGet:
+		w.Write([]byte("GET Method on Execs Route Page"))
+	case http.MethodPost:
+		w.Write([]byte("POST Method on Execs Route Page"))
+	case http.MethodPut:
+		w.Write([]byte("PUT Method on Execs Route Page"))
+	case http.MethodDelete:
+		w.Write([]byte("DELETE Method on Execs Route Page"))
+	default:
+		w.Write([]byte("Execs Route Page"))
+	}
+}
+
 func main() {
 
 	port := ":3000"
 
-	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-		// fmt.Fprintf(w, "Main Page")
-		w.Write([]byte("Main Page"))
-	})
-
-	http.HandleFunc("/teachers", func(w http.ResponseWriter, r *http.Request) {
-		// fmt.Fprintf(w, "Main Page")
-
-		switch r.Method {
-		case http.MethodGet:
-			w.Write([]byte("GET Method on Teachers Route Page"))
-		case http.MethodPost:
-
-			// Parse form data (necessary for x-www-form-urlencoded)
-
-			// err := r.ParseForm()
-			// if err != nil {
-			// 	http.Error(w, "Error parsing form", http.StatusBadRequest)
-			// 	return
-			// }
-			// fmt.Println("Form:", r.Form)
-
-			// // Prepare response data
-			// response := make(map[string]any)
-			// for key, value := range r.Form {
-			// 	response[key] = value[0]
-			// }
-			// fmt.Println("Processed Response Map:", response)
-
-			// name := r.FormValue("name")
-			// fmt.Println("Name: ", name)
-
-			// RAW Body
-
-			body, err := io.ReadAll(r.Body)
-			if err != nil {
-				return
-			}
-			defer r.Body.Close()
-
-			fmt.Println("RAW Body:", body)
-			fmt.Println("RAW Body:", string(body))
-
-			// If we expect json data, then unmarshal it.
-			var user user
-			err = json.Unmarshal(body, &user)
-			if err != nil {
-				return
-			}
-			fmt.Println("Unmarshaled JSON into an instance of user struct: ", user)
-			fmt.Println("Recieved user name as:", user.Name)
-
-			// // Prepare response data
-			response := make(map[string]any)
-			for key, value := range r.Form {
-				response[key] = value[0]
-			}
-
-			err = json.Unmarshal(body, &response)
-			if err != nil {
-				return
-			}
-			fmt.Println("Unmarshaled JSON into a map: ", response)
-
-			w.Write([]byte("POST Method on Teachers Route Page"))
-		case http.MethodPut:
-			w.Write([]byte("PUT Method on Teachers Route Page"))
-		case http.MethodDelete:
-			w.Write([]byte("DELETE Method on Teachers Route Page"))
-		default:
-			w.Write([]byte("Teachers Route Page"))
-		}
-
-	})
-
-	// http.HandleFunc("/students", func(w http.ResponseWriter, r *http.Request) {
-	// 	// fmt.Fprintf(w, "Main Page")
-	// 	w.Write([]byte("Students Page"))
-	// })
-
-	// http.HandleFunc("/execs", func(w http.ResponseWriter, r *http.Request) {
-	// 	// fmt.Fprintf(w, "Main Page")
-	// 	w.Write([]byte("Execs Page"))
-	// })
+	http.HandleFunc("/", rootHandler)
+	http.HandleFunc("/teachers/", teachersHandler)
+	http.HandleFunc("/students/", studentsHandler)
+	http.HandleFunc("/execs/", execsHandler)
 
 	fmt.Println("Server is running on port:", port)
 	err := http.ListenAndServe(port, nil)
