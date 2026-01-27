894c02e8e17f24669e0afa873ddc383bed8257dd
Alper Seyman <89801493+AlperSeyman@users.noreply.github.com>
2026-01-27 20:53:28 +0100

    updated cors.go file

--- School API Project/internal/api/middlewares/cors.go
+++ School API Project/internal/api/middlewares/cors.go
@@ -5,16 +5,41 @@ import (
 	"net/http"
 )
 
+// api is hosted at www.myapi.com
+// frontend server is at www.myfrontend.com
+
+// Allowed origins
+var allowedOrigins = []string{
+	"https://my-origin-url.com",
+	"https://www.myfrontend.com",
+	"http://localhost:3000",
+}
+
 func CORSMiddleware(next http.Handler) http.Handler {
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
 		origin := r.Header.Get("Origin")
 		fmt.Println(origin)
 
-		r.Header.Set("Access-Control-Allow-Headers", "Content-Type, Authorization")
-		r.Header.Set("Access-Control-Allow-Methods", "GET, POST, PUT, PATCH, DELETE")
-		r.Header.Set("Access-Control-Credentials", "true")
+		if isOriginAllowed(origin) {
+			w.Header().Set("Access-Control-Allow-Origin", origin)
+		} else {
+			http.Error(w, "Not allowed by CORS", http.StatusForbidden)
+		}
+
+		w.Header().Set("Access-Control-Allow-Headers", "Content-Type, Authorization")
+		w.Header().Set("Access-Control-Allow-Methods", "GET, POST, PUT, PATCH, DELETE")
+		w.Header().Set("Access-Control-Credentials", "true")
 		next.ServeHTTP(w, r)
 	})
+}
+
+func isOriginAllowed(origin string) bool {
+	for _, allowedOrigin := range allowedOrigins {
+		if origin == allowedOrigin {
+			return true
+		}
+	}
+	return false
 }
 
 /*
